9fa71d407afddc2cba0c07c264f9dbd3baaed8a1
Eric Trương <truongqk1987@Quangs-MacBook-Pro.local>
2020-09-23 22:19:51 +0700

    Init autocomplete

--- src/components/autocompleteInput/AutocompleteInput.js
+++ src/components/autocompleteInput/AutocompleteInput.js
@@ -1,5 +1,82 @@
-import React from 'react';
+import React from "react";
+import { createUseStyles } from "react-jss";
+import isEmpty from "lodash.isempty";
 
-const AutocompleteInput = () => <div />
+import { ReactComponent as SearchIcon } from "../../assets/images/search-icon.svg";
+
+const SearchBoxLayout = {
+  maxWidth: "30rem",
+  minWidth: "8rem",
+  padding: "0.5rem",
+};
+
+const useStyles = createUseStyles({
+  SearchBox: {
+    display: "flex",
+    alignItems: "center",
+    background: "#ffffff",
+    margin: "2rem auto 0.5rem auto",
+
+    ...SearchBoxLayout,
+
+    "& > .search-icon": {
+      width: "1.2rem",
+      height: "1.2rem",
+    },
+
+    "& > .search-input": {
+      border: "none",
+      padding: "0.15rem 0.5rem",
+      flexGrow: 1,
+      "&:focus": {
+        outline: "none",
+      },
+    },
+  },
+  Options: {
+    display: "block",
+    listStyle: "none",
+    ...SearchBoxLayout,
+    margin: "auto",
+    padding: 0,
+    maxWidth: '31rem',
+
+    '& > li.item': {
+        padding: '0.5rem',
+        background: '#ffffff'
+    }
+  },
+});
+
+const Options = ({ values, classes }) => (
+  <ul className={classes.Options}>
+    {values.map((value, index) => (
+      <li className="item" key={value} onClick={() => {}}>
+        {value}
+      </li>
+    ))}
+  </ul>
+);
+
+const AutocompleteInput = ({ suggestList, onChange, onKeyDown, value }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.SearchBox}>
+        <SearchIcon className="search-icon" />
+        <input
+          type="text"
+          className="search-input"
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={value}
+        />
+      </div>
+      {!isEmpty(suggestList) && (
+        <Options classes={classes} values={suggestList} />
+      )}
+    </>
+  );
+};
 
 export default AutocompleteInput;
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
+import debounce from 'lodash.debounce';
 
 import { HeaderMenu , AutocompleteInput } from '../../components';
+import AvailableCountryList from './mock-data';
 
 const HomeContainer = () => {
     const [ value, setValue ] = useState(false);
-    const onToggleDarkTheme = () => { debugger; setValue(!value)};
+    const [ userInput, setUserInput ] = useState('');
+    const [ suggestList, setSuggestList ] = useState([]);
+
+    const onKeyDown = useCallback((event) => {}, []);
+
+    const debounceUserInput = useRef(debounce((value) => {
+        const filterListByValue = AvailableCountryList.filter(
+            (countryName) =>
+              countryName.toLowerCase().indexOf(value.toLowerCase()) > -1
+          );
+        setSuggestList(filterListByValue.slice(0, 10));
+    }, 300)).current;
+
+    const onUserInputChanged = useCallback((event) => {
+        const updatedValue = event.target.value;
+        setUserInput(updatedValue);
+        debounceUserInput(updatedValue)
+    }, [debounceUserInput]);
+
+    const onToggleDarkTheme = () => { setValue(!value)};
     return <>
-    <HeaderMenu onToggleDarkTheme={onToggleDarkTheme} isDarkTheme={value} />
-    <AutocompleteInput />
-</>
+        <HeaderMenu onToggleDarkTheme={onToggleDarkTheme} isDarkTheme={value} />
+        <AutocompleteInput onKeyDown={onKeyDown} onChange={onUserInputChanged} value={userInput} suggestList={suggestList}/>
+    </>
 } 
 
 export default HomeContainer;
